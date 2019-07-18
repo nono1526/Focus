d74f725bbf093659a4fae1f524f808d2ff911222
Nono <nono@bxb.com>
2019-07-18 11:34:22 +0800

    Updates

---
+++ precache-manifest.009f53a9e6e672137a203219860af847.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "a284243652fb16d3db57",
+    "url": "/js/chunk-vendors.3187719c.js"
+  },
+  {
+    "revision": "cbe5d650369199af103c",
+    "url": "/js/app.55325f40.js"
+  },
+  {
+    "revision": "705253cdb6dd6a1a155f",
+    "url": "/js/about.0b6eeab2.js"
+  },
+  {
+    "revision": "6763674b550e9e797a9f09fede852ff1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "cbe5d650369199af103c",
+    "url": "/css/app.2971e3f1.css"
+  }
+];
