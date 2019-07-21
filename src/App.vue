e079d6f123c5d10b97dbd929718fd9c9b49529f9
nono1526 <nono1102137211@gmail.com>
2019-07-22 00:29:03 +0800

    Add setting

--- src/App.vue
+++ src/App.vue
@@ -8,12 +8,12 @@
     <div class="top-space "
       :class="actionClass"
     >
-      <button :class="actionClass" class="back" v-show="isEdit" @click="isEdit = false">
+      <button :class="actionClass" class="back" v-show="isEdit" @click="back">
         <img src="@/assets/left-arrow.svg"> Back
       </button>
       <h2 v-show="status === 'mid' && !isEdit" class="title">Start to focus</h2>
     </div>
-    <ul class="todos " :class="actionClass">
+    <ul class="todos " :class="actionClass" v-show="!isSetting">
       <transition-group>
         <li class="todos__tile"  @click="doneTask(item)" v-for="(item, key) in todos" :key="key" :class="{show: !item.done}">
           <div class="tile__btn" :class="{active: item.done}"></div>
@@ -21,18 +21,34 @@
         </li>
       </transition-group>
     </ul>
+    <div class="setting" :class="{show: isSetting}">
+      <h3>Focus time: {{ focusTime / 60 / 1000 }} minutes</h3>
+      <div class="setting__row">
+        <input v-model.number="newFocusTime" class="edit__textfield mr-10" type="text" placeHolder="Set focus time / min">
+        <f-btn  @click="setFocusTime">
+          SET
+        </f-btn>
+      </div>
+      <h3>Breath time: {{ breathTime / 1000 }} seconds</h3>
+      <div class="setting__row">
+        <input v-model.number="newRelaxTime" class="edit__textfield mr-10" type="text" placeHolder="Set breath time / sec">
+        <f-btn  @click="setRelaxTime">
+          SET
+        </f-btn>
+      </div>
+    </div>
     <div class="toolbar " :class="actionClass">
       <div class="toolbar__actions actions--main" v-show="!isPause && !isEdit">
         <img class="action__btn" src="@/assets/add.svg" v-show="isWaiting" @click="isEdit = true">
         <img class="action__btn" src="@/assets/pause.svg" v-show="!isWaiting" @click="togglePause">
         <img class="action__btn" src="@/assets/play.svg" v-show="isWaiting" @click="startTask" >
-        <img class="action__btn" src="@/assets/Settings.svg" v-show="isWaiting">
+        <img class="action__btn" src="@/assets/Settings.svg" v-show="isWaiting" @click="showSetting">
       </div>
       <div class="toolbar__actions actions--pause" v-show="isPause && !isEdit">
         <f-btn class="mr-10" @click="togglePause">Coutinue <img style="margin-left: 5px" src="@/assets/play-button.svg" width="15px"></f-btn>
         <f-btn class="ml-10" outline @click="stop">Quit</f-btn>
       </div>
-      <div class="toolbar__actions actions--edit" v-show="isEdit">
+      <div class="toolbar__actions actions--edit" v-show="isEdit && !isSetting">
         <input v-model="newTask" class="edit__textfield" type="text" placeHolder="Let's get things done" @keydown.enter="addTask">
         <img src="@/assets/add.svg" class="add__task" @click="addTask">
       </div>
@@ -55,6 +71,8 @@ export default {
   },
   data () {
     return {
+      newFocusTime: 1,
+      newRelaxTime: 30,
       newTask: '',
       isEdit: false,
       focusTime: MINUTE,
@@ -63,7 +81,8 @@ export default {
       counter: 25 * MINUTE,
       todos: [],
       countTimer: null,
-      isPause: false
+      isPause: false,
+      isSetting: false
     }
   },
   computed: {
@@ -90,8 +109,22 @@ export default {
     window.removeEventListener('beforeunload', e => this.beforeunloadHandler(e))
   },
   methods: {
+    setFocusTime () {
+      this.focusTime = this.newFocusTime * MINUTE
+    },
+    setRelaxTime () {
+      this.relax = this.newRelaxTime * 1000
+    },
+    back () {
+      this.isEdit = false
+      this.isSetting = false
+    },
     beforeunloadHandler () {
       window.localStorage.setItem('todos', JSON.stringify(this.todos.filter(todo => !todo.done)))
+    },
+    showSetting () {
+      this.isEdit = true
+      this.isSetting = true
     },
     addTask () {
       this.todos.push({
@@ -196,12 +229,16 @@ html, body
   background-color transparent
   border none
   outline none
+.setting
+  opacity 0
+  &.show
+    opacity 1
 .todos
   transition flex 1s
   &.mid
-    flex 5
+    flex 1
   &.top
-    flex 9
+    flex 4
   &.bottom
     flex 0
     opacity 0
@@ -267,17 +304,25 @@ html, body
     height 100%
     .action__btn
       margin auto 30px
-   .edit__textfield
-      flex 1
-      margin-left: 15px
-      min-height 30px
-      background-color transparent
-      border-radius 30px
-      border 2px solid rgba(255,255,255, 0.5)
-      font-size 18px
-      padding 3px 20px
-      outline none
-      color rgba(255,255,255, 0.7)
+
     .add__task
       margin auto 15px
+.edit__textfield
+  flex 1
+  margin-left: 15px
+  min-height 30px
+  background-color transparent
+  border-radius 30px
+  border 2px solid rgba(255,255,255, 0.5)
+  font-size 18px
+  padding 3px 20px
+  outline none
+  width 100%
+  color rgba(255,255,255, 0.7)
+.setting
+  padding 0 20px
+  h3
+    color rgba(255,255,255, 0.7)
+  .setting__row
+    display flex
 </style>
